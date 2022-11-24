fdb2c2afbbf8ddb5f55fcf5f9eae4442cebd6f93
Jansen <jan53n@outlook.in>
2022-11-24 20:29:40 +0530

    simple old dfs

---
+++ dfs/README.md
@@ -0,0 +1,10 @@
+# Deapth first search
+
+* pick an arbitary vertex as root
+* mark root vertex as visited
+* find all root vertex's adjacent vertices
+* iterate over the adjacent vertices
+    * on vertex that are not marked as visited recurse
+
+## References
+* https://en.wikipedia.org/wiki/Depth-first_search
---
+++ dfs/src/lib.rs
@@ -0,0 +1,44 @@
+use std::vec;
+
+pub type Graph = Vec<(i32, i32)>;
+
+fn dfs(root: i32, graph: &Graph, discovered: &mut Vec<i32>) {
+    discovered.push(root);
+
+    for adj in adjacent(root, &graph) {
+        if !discovered.contains(&adj) {
+            dfs(adj, graph, discovered);
+        }
+    }
+}
+
+fn adjacent(root: i32, graph: &Graph) -> Vec<i32> {
+    let mut adj = vec![];
+
+    for (r, l) in graph {
+        if r == &root {
+            adj.push(*l);
+        } else if l == &root {
+            adj.push(*r);
+        }
+    }
+
+    adj
+}
+
+pub fn traverse(root: i32, graph: Graph) -> Vec<i32> {
+    let mut discovered: Vec<i32> = vec![];
+    dfs(root, &graph, &mut discovered);
+    discovered
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let graph = vec![(1, 2), (1, 3), (1, 5), (2, 4), (2, 6), (3, 7), (5, 6)];
+        assert_eq!(traverse(1, graph), vec![1, 2, 4, 6, 5, 3, 7]);
+    }
+}
