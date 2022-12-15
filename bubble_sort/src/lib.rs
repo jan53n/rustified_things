743dc42aa1079f212c4773e365e87c69a0552bf2
Jansen <jan53n@outlook.in>
2022-12-15 18:37:07 +0530

    bubble sort

---
+++ bubble_sort/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn sort<T: Ord + Clone>(input: &mut Vec<T>) -> &Vec<T> {
+    let mut count = 0;
+
+    for a in 0..(input.len() - 1) {
+        let b = a + 1;
+
+        if &input[a] > &input[b] {
+            input.swap(a, b);
+            count += 1;
+        }
+    }
+
+    if count > 0 {
+        return input;
+    } else {
+        return sort(input);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut i = vec![5, 1, 7, 6];
+        let result = sort(&mut i);
+        assert_eq!(result, &vec![1, 5, 6, 7]);
+    }
+}
